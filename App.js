92471e1d2b467adafcd934a302462115ae7b2c4a
jgustine <jeremy.gustine@perceptivesoftware.com>
2014-07-01 09:55:12 -0500

    Add chart.

--- App.js
+++ App.js
@@ -17,34 +17,11 @@ Ext.define('CustomApp', {
     items:{ html:'<a href="https://help.rallydev.com/apps/2.0rc3/doc/">App SDK 2.0rc3 Docs</a>'},
     launch: function() {
 
-	this.excludedDates = [];
+		this.excludedDates = [];
 
-        //Write app code here
-        console.log('Our First App woot!');
         this._createDateFields();
 		this._addMultiDateCalendar();
-		this._createButtons();
     },
-
-    _createButtons: function() {
-		var getReportButton = Ext.create('Ext.Container', {
-			items: [{
-				xtype: 'rallybutton',
-				text: 'Get Report',
-				listeners: {
-					click: function(myStore, myData, success) {
-						var sDate = Rally.util.DateTime.toIsoString(this.startDate);
-						var eDate = Rally.util.DateTime.toIsoString(this.endDate);
-						this._loadData(sDate, eDate);
-					},
-				scope: this
-				}
-			}],
-			renderTo: Ext.getBody().dom,
-			scope: this
-		});
-		this.add(getReportButton);
-	},
 
     _createDateFields: function() {
 		var start = Ext.create('Ext.Container', {
@@ -159,11 +136,91 @@ Ext.define('CustomApp', {
                 }, this);
 
                 this._loadGrid(myStore, records);
+                this._createChart(records);
               },
               scope: this
           },
           fetch: ['FormattedID', 'Name', 'AcceptedDate', 'InProgressDate', 'RevisionHistory', 'Revisions', 'Description', 'User', 'Project']
       });
+    },
+
+    _createChart: function(records) {
+		var dict = {};
+   		for (var i = 0; i < records.length; i++) {
+   			var exclusions = records[i].DaysInProgressExclusions
+
+   			var m = 1;
+   			while (m < 100) {
+   				var multiple = 5 * m;
+   				if (exclusions <= multiple) {
+   					var begin;
+   					if (multiple == 5) begin = 0;
+   					else begin = multiple - 4;
+
+   					var name = 'Stories ' + begin + ' to ' + multiple;
+   					if (dict[multiple] == undefined) {
+   						dict[multiple] = { 'name': name, 'data': 1 };
+   					} else {
+   						var obj = dict[multiple];
+   						obj.data = obj.data + 1;
+   					}
+   					break;
+   				}
+   				if (m == 100) {
+   					//TODO
+   				}
+   				m++;
+   			}
+   		}
+
+   		var data = [];
+   		for (var prop in dict) {
+   			data.push(dict[prop]);
+   		}
+
+		var store = Ext.create('Ext.data.JsonStore', {
+		    fields: ['name', 'data'],
+		    data: data
+		});
+
+		var chart = Ext.create('Ext.chart.Chart', {
+		    renderTo: Ext.getBody(),
+		    width: 500,
+		    height: 350,
+		    animate: true,
+		    store: store,
+		    theme: 'Base:gradients',
+		    series: [{
+		        type: 'pie',
+		        angleField: 'data',
+		        showInLegend: true,
+		        tips: {
+		            trackMouse: true,
+		            width: 140,
+		            height: 28,
+		            renderer: function(storeItem, item) {
+		                // calculate and display percentage on hover
+		                var total = 0;
+		                store.each(function(rec) {
+		                    total += rec.get('data');
+		                });
+		                this.setTitle(storeItem.get('name') + ': ' + Math.round(storeItem.get('data') / total * 100) + '%');
+		            }
+		        },
+		        highlight: {
+		            segment: {
+		                margin: 20
+		            }
+		        },
+		        label: {
+		            field: 'name',
+		            display: 'rotate',
+		            contrast: true,
+		            font: '18px Arial'
+		        }
+		    }]
+		});
+		this.add(chart);
     },
 
     // Create and Show a Grid of given stories
@@ -211,78 +268,80 @@ Ext.define('CustomApp', {
     },
 
     _addMultiDateCalendar:function() {
-    	console.log("blah");
-    	//Ext.onReady(function() {
-		    Ext.tip.QuickTipManager.init();
+	    Ext.tip.QuickTipManager.init();
 
-			var store, panel;
+		var store, panel;
 
-			panel = Ext.create('Ext.form.Panel', {
-		        width:	200,
-		        height: 200,
-		        
-		        id: 'formPanel',
-		        
-		        layout: 'vbox',
-		        
-		        defaults: {
-		            autoScroll: true,
-		            bodyPadding: 8,
-					listeners: {
-						specialkey: function(form, event) {
-							if (event.getKey() === event.ENTER) {
-								form.up().down('#validateButton').handler();
-							};
-						}
+		panel = Ext.create('Ext.form.Panel', {	        
+	        id: 'formPanel',
+	        
+	        layout: 'vbox',
+	        frame: false,
+			border: false,
+	        
+	        defaults: {
+	            autoScroll: true,
+	            bodyPadding: 8,
+				listeners: {
+					specialkey: function(form, event) {
+						if (event.getKey() === event.ENTER) {
+							form.up().down('#validateButton').handler();
+						};
 					}
-		        },
-		        
-		        position: 'absolute',
-		        x:  20,
-		        y:  20,
-		        
-		        items: [{
-		            xtype: 'multidatefield',
-		            id: 'multiDateField',
-		            allowBlank: false,
-		            multiValue: true,
-		            submitFormat: 'Y-m-d',
-		            submitRangeSeparator: '/',
-		        }, {
-		            xtype: 'button',
-		            id:    'validateButton',
-		            text:  'Validate',
-		            scope: this,
-		            handler: function() {
-		                var form, field;
-		                
-		                form  = Ext.getCmp('formPanel').getForm();
-		                field = Ext.getCmp('multiDateField');
-		                
-		                if ( form.isValid() ) {
-		                    var values = form.getValues();
-		                    var valuesString = field.getSubmitValue();
-		                    var valuesArray = field.expandValues(valuesString, 'Y-m-d', ';', '/');
-		                    this.excludedDates =  _.map(valuesArray, function(date) {
-				        		return date.toString();
-				        	});
-		                    alert('Form is valid: ' + Ext.JSON.encode(values[ field.inputId ]));
-		                }
-		                else {
-		                    alert('Form is invalid');
-		                };
-		            }
-		        }],
-		        
-		        renderTo: Ext.getBody()
-			});
+				}
+	        },
+	        
+	        items: [{
+	            xtype: 'multidatefield',
+	            id: 'multiDateField',
+	            fieldLabel: 'Exclusions',
+	            allowBlank: false,
+	            multiValue: true,
+	            submitFormat: 'Y-m-d',
+	            submitRangeSeparator: '/',
+	        }, {
+	            xtype: 'button',
+	            id:    'validateButton',
+	            text:  'Get Report',
+	            scope: this,
+	            handler: function() {
+	                var form, field;
+	                
+	                form  = Ext.getCmp('formPanel').getForm();
+	                field = Ext.getCmp('multiDateField');
+	                
+	                if ( form.isValid() ) {
+	                    var values = form.getValues();
+	                    var valuesString = field.getSubmitValue();
+	                    var valuesArray = field.expandValues(valuesString, 'Y-m-d', ';', '/');
+	                    this.excludedDates =  _.map(valuesArray, function(date) {
+			        		return date.toString();
+			        	});
+			        	this._runReport();                    
+	                }
+	                else {
+	                    this._runReport(); 
+	                };
+	            }
+	        }],
+	        
+	        renderTo: Ext.getBody()
+		});
 
-			this.add(panel);
-		//});
+		this.add(panel);
+    },
+
+    _runReport: function() {
+	    var sDate = Rally.util.DateTime.toIsoString(this.startDate);
+		var eDate = Rally.util.DateTime.toIsoString(this.endDate);
+		this._loadData(sDate, eDate);
     }
 });
 
-//add a chart 
+
 //replace grid
 //fix switching months on calendar
+//change initial dates to mean only that a story was accepted between start and end date?
 
+
+//https://github.com/nohuhu/Ext.ux.form.field.MultiDate
