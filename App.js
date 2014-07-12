880ccfbdf73a6fa67af3f21a06fff35d0cb3535b
jgustine <jeremy.gustine@perceptivesoftware.com>
2014-07-12 00:16:37 -0500

    Split calendar creation into another file.

--- App.js
+++ App.js
@@ -332,85 +332,8 @@ Ext.define('CustomApp', {
     },
 
     _addMultiDateCalendar:function() {
-	    Ext.tip.QuickTipManager.init();
-
-		var store, panel;
-
-		panel = Ext.create('Ext.form.Panel', {	        
-	        id: 'formPanel',
-	        
-	        layout: 'vbox',
-	        frame: false,
-			border: false,
-	        
-	        defaults: {
-	            autoScroll: true,
-	            bodyPadding: 8,
-				listeners: {
-					specialkey: function(form, event) {
-						if (event.getKey() === event.ENTER) {
-							form.up().down('#validateButton').handler();
-						};
-					}
-				}
-	        },
-	        
-	        items: [{
-		            xtype: 'multidatefield',
-		            id: 'multiDateField',
-		            fieldLabel: 'Exclusions',
-		            allowBlank: true,
-		            multiValue: true,
-		            submitFormat: 'Y-m-d',
-		            submitRangeSeparator: '/',
-	        	}, 
-				{
-		            xtype: 'fieldcontainer',
-		            fieldLabel: 'Exclude Weekends',
-		            defaultType: 'checkboxfield',
-		            items: [
-		                {
-		                    name      : 'weekendCheckbox',
-		                    inputValue: '1',
-		                    checked   : true,
-		                    id        : 'weekendCheckbox'
-						}
-		            ]
-		        },
-                {
-	            xtype: 'button',
-	            id:    'validateButton',
-	            text:  'Get Report',
-	            scope: this,
-	            handler: function() {
-	                var form, field;
-	                
-	                form  = Ext.getCmp('formPanel').getForm();
-	                field = Ext.getCmp('multiDateField');
-	                
-	                if ( form.isValid() ) {
-	                    var values = form.getValues();
-	                    var valuesString = field.getSubmitValue();
-	                    var valuesArray = field.expandValues(valuesString, 'Y-m-d', ';', '/');
-
-						this.checkboxValue = Ext.getCmp('weekendCheckbox').getValue();
-
-		        		this.excludedDates =  _.map(valuesArray, function(date) {
-		        			return date.toString();
-		        		});			        	
-
-			        	this._runReport();                    
-	                }
-	                else {
-	                	console.log("The form is invalid.");
-	                	//alert to the screen?
-	                    this._runReport(); 
-	                };
-	            }
-	        }],
-	        
-	        renderTo: Ext.getBody()
-		});
+    	var calendar = Ext.create('MultiDateCalendar');
+    	var panel = calendar._create(this);
 
 		this.topLeftContainer.add(panel);
     },
@@ -433,3 +356,5 @@ Ext.define('CustomApp', {
 
 //split into separate files:
 //http://stackoverflow.com/questions/20617913/how-to-break-up-a-custom-app-into-multiple-js-files
+
+//pass callbacks into other files
